6681bb4932fac82aff52d8966af17e5df4b5cc62
amyspencerproject <amy@spencerproject.com>
2022-07-08 13:27:57 -0600

    Committing before moving the project folder out of this repo just in case that is not what I am supposed to do

--- README_2.md
+++ README_2.md
@@ -1,6 +1,22 @@
-## Amy's Notes on Tutuorial 
-Installed/ ran the following command 
-> npx create-react-app react-hooks-crud
-Not sure if this react-hooks-crud project folder should be inside the crud-tutorial folder or not. The tutorial directions are not explicit only saying to 
->Open cmd at the folder you want to save Project folder
+# Amy's Notes for this Tutorial
+
+## Install
+Lots of confusion as to if the react-hooks-crud project folder should be inside of the tutorial folder. Instructions were not clear and it seems weird to me to have a project folder with its own src and node_modules, etc under a folder with the it's own src/ node_modules, etc. Moved it out and then back when directions called for modifying the tutorial.service.js file which is most certainly part of the crud-tutorial. 
+
+Also begining to udnerstand that when you do installs in command line there will always be loads of warnings of various levels but unless the install is dead stopped just press forward. Especially because this is just a tutorial.
+
+Changed http-common.js files in both the tutorial and the project folder. Instructions didn't specify which to change.
+
+### Create Data Service
+In this step a data service is created that uses an axios object to send HTTP requests
+
+_C_reate: create
+_R_etreive: getAll, get
+_U_pdate: update
+_D_elete: remove, removeAll
+and 
+FINDER: findByTitle
+
+
+
 
--- src/services/tutorial.service.js
+++ src/services/tutorial.service.js
@@ -1,33 +1,68 @@
 import http from "../http-common";
+const getAll = () => {
+  return http.get("/tutorials");
+};
+const get = id => {
+  return http.get(`/tutorials/${id}`);
+};
+const create = data => {
+  return http.post("/tutorials", data);
+};
+const update = (id, data) => {
+  return http.put(`/tutorials/${id}`, data);
+};
+const remove = id => {
+  return http.delete(`/tutorials/${id}`);
+};
+const removeAll = () => {
+  return http.delete(`/tutorials`);
+};
+const findByTitle = title => {
+  return http.get(`/tutorials?title=${title}`);
+};
+const TutorialService = {
+  getAll,
+  get,
+  create,
+  update,
+  remove,
+  removeAll,
+  findByTitle
+};
+export default TutorialService;
 
-class TutorialDataService {
-  getAll() {
-    return http.get("/tutorials");
-  }
 
-  get(id) {
-    return http.get(`/tutorials/${id}`);
-  }
+// original file before installing axios
+// import http from "../http-common";
 
-  create(data) {
-    return http.post("/tutorials", data);
-  }
+// class TutorialDataService {
+//   getAll() {
+//     return http.get("/tutorials");
+//   }
 
-  update(id, data) {
-    return http.put(`/tutorials/${id}`, data);
-  }
+//   get(id) {
+//     return http.get(`/tutorials/${id}`);
+//   }
 
-  delete(id) {
-    return http.delete(`/tutorials/${id}`);
-  }
+//   create(data) {
+//     return http.post("/tutorials", data);
+//   }
 
-  deleteAll() {
-    return http.delete(`/tutorials`);
-  }
+//   update(id, data) {
+//     return http.put(`/tutorials/${id}`, data);
+//   }
 
-  findByTitle(title) {
-    return http.get(`/tutorials?title=${title}`);
-  }
-}
+//   delete(id) {
+//     return http.delete(`/tutorials/${id}`);
+//   }
 
-export default new TutorialDataService();
+//   deleteAll() {
+//     return http.delete(`/tutorials`);
+//   }
+
+//   findByTitle(title) {
+//     return http.get(`/tutorials?title=${title}`);
+//   }
+// }
+
+// export default new TutorialDataService();
